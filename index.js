d633d813f762ae7602f9fd302d39966094d2af20
Luiz Henrique <lmenezesmessias@gmail.com>
2021-11-03 14:44:03 -0300

    Finalização

---
+++ index.js
@@ -0,0 +1,4 @@
+function mudarImagem() {
+    let imagem = window.document.getElementById('imagem');
+    imagem.src = "http://www.ignicaodigital.com.br/wp-content/uploads/2014/02/facebook-promocao.jpg";
+}
